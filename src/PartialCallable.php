b20e5ca803457ef31770b7f522648ed9c890aea2
USAMI Kenta <tadsan@zonu.me>
2015-01-21 21:09:04 +0900

    Init repository

---
+++ src/PartialCallable.php
@@ -0,0 +1,46 @@
+<?php
+namespace Teto\Functools;
+
+/**
+ * array_map compatible partial application wrapper
+ *
+ * @package    Teto
+ * @subpackage Functools
+ * @author     USAMI Kenta <tadsan@zonu.me>
+ * @copyright  2015 USAMI Kenta
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ */
+final class PartialCallable
+{
+    /** @var callable */
+    private $callable;
+    /** @var mixed[] */
+    private $arguments;
+    /** @var int|null */
+    private $pos;
+
+    public function __construct(callable $callable, array $arguments = [], $pos = null)
+    {
+        $this->callable  = $callable;
+        $this->arguments = $arguments;
+        $this->pos       = $pos;
+    }
+    
+    public function __invoke()
+    {
+        return call_user_func_array($this->callable, $this->arguments(func_get_args()));
+    }
+
+    private function arguments(array $additional_arguments)
+    {
+        $arguments = $this->arguments;
+        if ($this->pos !== null) {
+            $arguments[$this->pos] = array_shift($additional_arguments);
+            ksort($arguments);
+        } else {
+        	$arguments = array_merge($arguments, $additional_arguments);
+        }
+        
+        return $arguments;
+    }
+}
---
+++ tests/PartialCallableTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Teto\Functools;
+
+final class FunctoolsTest extends \PHPUnit_Framework_TestCase
+{
+    /** @dataProvider dataProviderFor_array_map */
+    public function test_with_array_map($expected, $callable, $default_args, $pos, $input)
+    {
+        $patial_applicated = new PartialCallable($callable, $default_args, $pos);
+        $actual = array_map($patial_applicated, $input);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function dataProviderFor_array_map()
+    {
+        return [
+            [
+                'expected' => [
+                    ['a', 'b', 'c'],
+                    ['d', 'e', 'f'],
+                ],
+                'callable' => 'array_column',
+                'default_args' => [1 => "hoge"],
+                'pos' => 0,
+                'input' => [
+                    [
+                        ['hoge' => "a", "fuga" => "1"],
+                        ['hoge' => "b", "fuga" => "1"],
+                        ['hoge' => "c", "fuga" => "1"],
+                    ],
+                    [
+                        ['hoge' => "d", "fuga" => "1"],
+                        ['hoge' => "e", "fuga" => "1"],
+                        ['hoge' => "f", "fuga" => "1"],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
